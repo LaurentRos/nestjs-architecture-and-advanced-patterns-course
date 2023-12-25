2ae6c312d132b0b5c14dd3da58a5b71c87160e46
Laurent Ros <laurent@mediflash.fr>
2023-12-25 19:14:02 +0100

    sagas

--- src/alarms/alarms.module.ts
+++ src/alarms/alarms.module.ts
@@ -7,6 +7,9 @@ import { GetAlarmsQueryHandler } from 'src/alarms/application/queries/get-alarms
 import { AlarmCreatedEventHandler } from 'src/alarms/application/event-handlers/alarm-created.event-handler';
 import { AcknowledgeAlarmCommandHandler } from 'src/alarms/application/commands/acknowledge-alarm.command-handler';
 import { AlarmAcknowledgedEventHandler } from 'src/alarms/application/event-handlers/alarm-acknowledged.event-handler';
+import { CascadingAlarmsSaga } from 'src/alarms/application/sagas/cascading-alarms.saga';
+import { NotifyFacilitySupervisorCommandHandler } from 'src/alarms/application/commands/notify-facility-supervisor.command-handler';
+import { UnacknowledgedAlarmsSaga } from 'src/alarms/application/sagas/unacknowledged-alarms.saga';
 
 @Module({
   controllers: [AlarmsController],
@@ -18,6 +21,9 @@ import { AlarmAcknowledgedEventHandler } from 'src/alarms/application/event-hand
     AlarmCreatedEventHandler,
     AcknowledgeAlarmCommandHandler,
     AlarmAcknowledgedEventHandler,
+    CascadingAlarmsSaga,
+    NotifyFacilitySupervisorCommandHandler,
+    UnacknowledgedAlarmsSaga,
   ],
 })
 export class AlarmsModule {
